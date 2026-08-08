ab080107f57856795b30d89bd834327e068415d3
Hakan Özdemir <runcleancode@gmail.com>
2026-08-08 11:30:36 +0300

    feat: build initial in-memory city API

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,38 @@
+using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        private readonly CitiesDataStore _citiesDataStore;
+
+        //Dependency Injection
+        public CitiesController(CitiesDataStore citiesDataStore)
+        {
+            //null-coalescing operator
+            _citiesDataStore = citiesDataStore
+                ?? throw new ArgumentNullException(nameof(citiesDataStore));
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CityDto>> GetCities()
+        {
+            return Ok(_citiesDataStore.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCity(int id)
+        {
+            var cityToReturn = _citiesDataStore.Cities
+                .FirstOrDefault(c => c.Id == id);
+
+            if (cityToReturn is null)
+                return NotFound();
+
+            return Ok(cityToReturn);
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    public class PointsOfInterestController : ControllerBase
+    {
+        private readonly CitiesDataStore _citiesDataStore;
+
+        public PointsOfInterestController(CitiesDataStore citiesDataStore)
+        {
+            _citiesDataStore = citiesDataStore
+                ?? throw new ArgumentNullException(nameof(citiesDataStore));
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = _citiesDataStore.Cities
+                .FirstOrDefault(c => c.Id == cityId);
+
+            if (city is null)
+                return NotFound();
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int id)
+        {
+            var city = _citiesDataStore.Cities
+                .FirstOrDefault(c => c.Id == cityId);
+
+            if (city is null)
+                return NotFound();
+
+            var pointOfInterest = city.PointsOfInterest
+                .FirstOrDefault(p => p.Id == id);
+
+            if (pointOfInterest is null)
+                return NotFound($"Id : {id} was not found.");
+
+            return Ok(pointOfInterest);
+        }
+    }
+}
