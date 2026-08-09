5f8a20266e2f4ce971e495a14c7763d266d95c38
Hakan Özdemir <runcleancode@gmail.com>
2026-08-09 15:03:14 +0300

    Feature/03 services and logging (#3)
    * feat: add IMailService, Local/CloudMailService and mail configuration
    
    * feat: set up Serilog logging infrastructure
    
    * feat: add file download endpoints to FilesController

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,16 +1,35 @@
 using CityInfo.API;
+using CityInfo.API.Services;
+using Microsoft.AspNetCore.StaticFiles;
+using Serilog;
+
+
+//Logger implementation
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-//Later Written
+//Custom Services
 builder.Services.AddSingleton<CitiesDataStore>();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
+
+//c# preprocessor directive
+#if DEBUG
+builder.Services.AddTransient<IMailService, LocalMailService>();
+#else 
+builder.Services.AddTransient<IMailService,CloudMailService>();
+#endif
 
 var app = builder.Build();
 
