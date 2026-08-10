d2d6cbd9f5770ddb906e15360a2ff845bdd79e4e
Hakan Özdemir <runcleancode@gmail.com>
2026-08-10 17:31:31 +0300

    feat: add City and PointOfInterest entity classes

---
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+        public City? City { get; set; }
+        public int CityId { get; set; }
+    }
+}
