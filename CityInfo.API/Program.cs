d4411e9e583c0024430db280c5dcf8e147a8b5c9
Hakan Özdemir <runcleancode@gmail.com>
2026-08-14 14:57:58 +0300

    refactor: update CitiesController to use repository and AutoMapper

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -37,6 +37,8 @@ builder.Services.AddDbContext<CityInfoContext>(options => options.UseSqlite(buil
 
 builder.Services.AddScoped<ICityInfoRepository, CityInfoRepository>();
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
